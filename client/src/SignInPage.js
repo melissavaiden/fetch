ee4956f670797959678432060cbc694f971d0925
Melissa Vaiden <melissavaiden@gmail.com>
2023-01-12 20:51:56 -0500

    Started Repo and Created Sign In Page

---
+++ client/src/SignInPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function SignInPage() {
+  return (
+    <div>
+        <h1>Welcome</h1>
+        <h3>Username</h3>
+        <input type='text'></input>
+        <br></br>
+        <h3>Password</h3>
+        <input type='text'></input>
+        <br></br>
+        <br></br>
+        <button>Sign In</button>
+        <br></br>
+        <button>Sign Up</button>
+    </div>
+  )
+}
