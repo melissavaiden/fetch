ae56215def4dc854ff095dc8c35c637240fc4cd6
Melissa Vaiden <melissavaiden@gmail.com>
2023-01-20 19:43:35 -0500

    add post request for login authentication on client side'

--- client/src/components/SignInPage.js
+++ client/src/components/SignInPage.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NavLink} from 'react-router-dom'
 
 export default function SignInPage() {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
 
   function handleSubmit() {
+    fetch('http://localhost:4000/', {
+      method: 'POST', 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username" : username,
+        "password" : password
+      })
+    })
+    .then((r) => r.json())
+    .then((newUser) => console.log(newUser))
 
+  }
+
+  function handleUserChange(e) {
+    setUsername(e.target.value)
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value)
   }
   return (
     <div className='container'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-floating">
-          <input type='username' className='form-control' placeholder='username'></input>
+          <input type='username' className='form-control' placeholder='username' onChange={handleUserChange}></input>
           <label>Username</label>
           <br></br>
         </div>
         <div className='form-floating'>
-          <input type='text' className='form-control' placeholder='password'></input>
+          <input type='text' className='form-control' placeholder='password' onChange={handlePasswordChange}></input>
           <label className='form-label'>Password</label>
         </div>
+        <br></br>
+        <button className='btn btn-primary'>Sign In</button>
       </form>
 
-      <br></br>
-      <br></br>
-      <button className='btn btn-primary'>Sign In</button>
       <br></br>
       <div className='container'>
         <label>Don't have an account?</label>
