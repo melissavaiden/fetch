bb00d640c67bf984bc83db80184fef1b04e260a5
Melissa Vaiden <melissavaiden@gmail.com>
2023-01-24 17:41:32 -0500

    Fixed error in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :ok 
     else
